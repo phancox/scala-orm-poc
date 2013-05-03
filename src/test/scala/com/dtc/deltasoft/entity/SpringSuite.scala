71cf4f78844af1a81256c4d32a734aa795022da9
Peter Hancox <phancox@dtc.com.au>
2013-05-03 11:19:24 +1000

    Added tests for Spring Beans capability.

---
+++ src/test/scala/com/dtc/deltasoft/entity/SpringSuite.scala
@@ -0,0 +1,26 @@
+package com.dtc.deltasoft.entity
+
+import org.junit.runner.RunWith
+import org.scalatest.junit._
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+import org.springframework.context.ApplicationContext
+import org.springframework.context.support.ClassPathXmlApplicationContext
+
+/**
+ * Unit test suite for Spring framework integration.
+ *
+ */
+@RunWith(classOf[JUnitRunner])
+class SpringSpec extends FunSpec with ShouldMatchers {
+
+  describe("The Spring framework") {
+    describe("should support loading the application context") {
+      it("from XML configuration file") {
+        val ctx: ApplicationContext = new ClassPathXmlApplicationContext("Beans.xml");
+        val suburb = ctx.getBean("suburbLongueville")
+        suburb.toString should equal("Longueville, NSW 2066, Australia")
+      }
+    }
+  }
+}
