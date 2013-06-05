de241eb8863ce9d1cfa91186dc306275d503154e
Peter Hancox <phancox@dtc.com.au>
2013-06-05 19:12:56 +1000

    Implement ConfigurationEventListener, ConfigurationErrorListener, and log initial configuration properties.

--- src/main/scala/com/dtc/deltasoft/Config.scala
+++ src/main/scala/com/dtc/deltasoft/Config.scala
@@ -1,8 +1,29 @@
 package com.dtc.deltasoft
 
+import scala.collection.JavaConversions._
 import java.io.File
 
 import org.apache.commons.configuration._
+import org.apache.commons.configuration.event._
+
+/**
+ * Listener for logging configuration events.
+ *
+ */
+object ConfigurationListener extends ConfigurationListener with ConfigurationErrorListener with Logging {
+  def configurationChanged(event: ConfigurationEvent) = {
+    if (!event.isBeforeUpdate()) {
+      val msg = s"ConfigurationEvent[${event.getType}]:" +
+        { if (event.getPropertyName != null) s" PropertyName=${event.getPropertyName}" } +
+        { if (event.getPropertyValue != null) s" PropertyValue=${event.getPropertyValue}" }
+      info(msg)
+    }
+  }
+  def configurationError(event: ConfigurationErrorEvent) = {
+    val msg = s"ConfigurationErrorEvent[${event.getType}]"
+    error(msg)
+  }
+}
 
 /**
  * Standard configuration management for DeltaSoft applications.
@@ -17,14 +38,14 @@ object Config extends Logging {
    * Returns a [[CombinedConfiguration]] configured as per the "config.xml" file located on the
    * class path.  If any exceptions are thrown while processing the configuration file, a default
    * [[CombinedConfiguration]] is returned that is initialised with values defined in the process
-   * environment (i.e., environment variables) and the JVM system properties. 
-   * 
+   * environment (i.e., environment variables) and the JVM system properties.
+   *
    */
   lazy val config = {
     val config = try {
       defaultConfigurationBuilder.getConfiguration(true)
     } catch {
-      case e: Throwable => {
+      case e: ConfigurationException => {
         error(e.withStackTrace)
         val config = new CombinedConfiguration()
         config.addConfiguration(new SystemConfiguration)
@@ -32,8 +53,16 @@ object Config extends Logging {
         config
       }
     }
+    config.addConfigurationListener(ConfigurationListener)
+    config.addErrorListener(ConfigurationListener)
     def logConfig(key: String) = { info(key + "=" + config.getString(key)) }
+    info("*" * 80)
     logConfig("DSTA_CONFIG")
+    info("*" * 80)
+    for {
+      key <- config.getKeys()
+    } logConfig(key)
+    info("*" * 80)
     config
   }
 }
